c855bdad0474269226245090b8d1ca76808eaf04
nonazakoyan <61244140+nonazakoyan@users.noreply.github.com>
2022-11-21 17:52:30 +0400

    Add files via upload

---
+++ BonusProject.py
@@ -0,0 +1,31 @@
+from time import sleep
+
+def foo(lst):
+	lst[2] = str(int(lst[2]) - 1)
+
+def countdown(ListInp, count):
+	for i in range(count):
+		if ListInp[2] == ListInp[1] == ListInp[0] == '00' :
+			ListInp[0] = '23'
+			ListInp[1] = '59'
+			ListInp[2] = '59'
+		elif ListInp[2] == ListInp[1] == '00':
+			ListInp[1] = '59'
+			ListInp[2] = '59'
+		elif ListInp[2] == '00':
+			ListInp[2] = '59'
+		else:
+			foo(ListInp)
+		print(":".join(ListInp))
+		sleep(1)
+
+
+inp = input("Insert time to countdown style. (00:00:00)\n>>> ").strip()
+count = input("Insert score of countdown\n>>> ").strip()
+
+
+if (len(inp) == 3 and 0 <= int(inp[0]) <= 23 and 0 <= int(inp[1]) <= 59 and 
+	0 <= int(inp[2]) <= 59 and count.isdigit()):
+	countdown(inp, int(count))
+else:
+	print("invalid input")
